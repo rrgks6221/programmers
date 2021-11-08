fa433fa8e25f148ffdd78cca782e6364115977a0
lsh <rrgks@naver.com>
2021-11-08 11:52:30 +0900

    약수의 합 풀이

---
+++ 1단계/약수의-합.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  let answer = 0;
+  
+  if (n < 2) return n;
+  
+  for (let i = 1; i <= n; i++) {
+      if (n % i === 0) {
+          answer += i;
+      }
+  }
+  return answer;
+}
