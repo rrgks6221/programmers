d1844848fd5ec766beb5355edbc032bf28f52c1a
lsh <rrgks@naver.com>
2021-12-06 12:23:48 +0900

    문자열 내 p와 y의 개수 풀이 수정

--- 1단계/문자열-내-p와-y의-개수.js
+++ 1단계/문자열-내-p와-y의-개수.js
@@ -1,13 +1,3 @@
-function solution(s){
-    var count = 0;
-    for(var i = 0; i < s.length;  i++)
-        if (s[i] == "p" || s[i] == "P"){
-            count++;
-        }
-        else if(s[i] == "y" || s[i] == "Y")
-            {
-            count--;
-        }
-
-    return count == 0;
+function solution(s) {
+  return (s.match(/p/gi) || []).length === (s.match(/y/gi) || []).length;
 }
