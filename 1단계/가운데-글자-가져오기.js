7900daee424e35b244b24533e0c13cb7c7dfcf41
lsh <rrgks@naver.com>
2021-12-07 10:21:58 +0900

    가운데 글자 가져오기 풀이 수정

--- 1단계/가운데-글자-가져오기.js
+++ 1단계/가운데-글자-가져오기.js
@@ -1,4 +1,5 @@
 function solution(s) {
-  var answer = '';
-  return s.length % 2 ? s[parseInt(s.length / 2)] : s[s.length / 2 - 1] + s[s.length / 2];
+  return s.length % 2
+    ? s[parseInt(s.length / 2)]
+    : s[s.length / 2 - 1] + s[s.length / 2];
 }
