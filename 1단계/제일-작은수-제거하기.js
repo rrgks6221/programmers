43668a334fef20d4ece8217c7b7336b44ccfd51d
lsh <rrgks@naver.com>
2021-12-13 11:54:53 +0900

    제일 작은수 제거하기 풀이 수정

--- 1단계/제일-작은수-제거하기.js
+++ 1단계/제일-작은수-제거하기.js
@@ -1,10 +1,6 @@
 function solution(arr) {
   const min = Math.min(...arr);
-  let count = arr.filter(element => min === element).length;
-  for (let i = 0; i < count; i++) {
-      let minIndex = arr.indexOf(min);
-      arr.splice(minIndex, 1);
-  }
-  return arr.length !== 0 ? arr : [-1];
+  const result = arr.filter((el) => min < el);
 
+  return result.length !== 0 ? result : [-1];
 }
