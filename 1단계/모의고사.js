2590442f5c7f9428a044400d366f5408b8a4e1bc
lsh <rrgks@naver.com>
2021-12-14 13:29:07 +0900

    모의고사 풀이 수정

--- 1단계/모의고사.js
+++ 1단계/모의고사.js
@@ -1,19 +1,23 @@
 function solution(answers) {
-  let answer = [];
-  let count = [0, 0, 0];
-  const answerOne = [1, 2, 3, 4, 5];
-  const answerTwo = [2, 1, 2, 3, 2, 4, 2, 5];
-  const answerThree = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-  for(let i = 0; i < answers.length; i++) {
-      if (answerOne[i % 5] === answers[i]) count[0]++;
-      if (answerTwo[i % 8] === answers[i]) count[1]++;
-      if (answerThree[i % 10] === answers[i]) count[2]++;
-  }
-  const max = Math.max(...count);
-  for (let i = 0; i < 3; i++) {
-      if (count[i] === max) {
-          answer.push(i + 1);
-      }
-  }
-  return answer;
+  const result = [];
+  const answersCnt = [0, 0, 0];
+  const ways = {
+    one: [1, 2, 3, 4, 5],
+    two: [2, 1, 2, 3, 2, 4, 2, 5],
+    three: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+  };
+
+  Object.values(ways).forEach((way, wayIdx) => {
+    answers.forEach((answer, answerIdx) => {
+      if (way[answerIdx % way.length] === answer) answersCnt[wayIdx] += 1;
+    });
+  });
+
+  const max = Math.max(...answersCnt);
+
+  answersCnt.forEach((cnt, idx) => {
+    if (cnt >= max) result.push(idx + 1);
+  });
+
+  return result.sort((a, b) => a - b);
 }
