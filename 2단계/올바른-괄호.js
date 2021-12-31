c8fc41b4c966b25443996801f192b2a284bae9b9
lsh <rrgks@naver.com>
2021-12-31 16:00:32 +0900

    올바른 괄호 풀이

---
+++ 2단계/올바른-괄호.js
@@ -0,0 +1,9 @@
+function solution(s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i += 1) {
+    if (s[i] === '(') count += 1;
+    else count -= 1;
+    if (count < 0) return false;
+  }
+  return count === 0;
+}
