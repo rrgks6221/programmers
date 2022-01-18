93158e3205ee3647f3893aa319a7196fbe8c875f
lsh <rrgks@naver.com>
2022-01-18 22:37:26 +0900

    짝지어 제거하기 풀이

---
+++ 2단계/짝지어-제거하기.js
@@ -0,0 +1,13 @@
+function solution(s) {
+  if (s.length % 2 === 1) return 0;
+
+  const stack = [];
+
+  for (let i = 0; i < s.length; i += 1) {
+    if (stack.length === 0 || stack[stack.length - 1] !== s[i]) {
+      stack.push(s[i]);
+    } else stack.pop();
+  }
+
+  return stack.length === 0 ? 1 : 0;
+}
