99df37ce178fd375f3f43acf555173a19f5a67c9
lsh <rrgks@naver.com>
2022-03-19 16:48:16 +0900

    다리를-지나는-트럭 풀이 수정

--- 2단계/다리를-지나는-트럭.js
+++ 2단계/다리를-지나는-트럭.js
@@ -1,28 +1,27 @@
 function solution(bridge_length, weight, truck_weights) {
   const truckOnBridge = [];
   const truckOnBridgeTime = [];
-  let minTime = 0;
+  let result = 0;
 
   while (truck_weights.length || truckOnBridge.length) {
-    for (let i = 0; i < truckOnBridgeTime.length; i += 1)
+    for (let i = 0; i < truckOnBridgeTime.length; i += 1) {
       truckOnBridgeTime[i] -= 1;
+    }
 
     if (truckOnBridgeTime[0] === 0) {
       truckOnBridge.shift();
       truckOnBridgeTime.shift();
     }
 
-    const weightOnBridge = truckOnBridge.reduce(
-      (sum, current) => (sum += current),
-      0
-    );
+    const weightOnBridge = truckOnBridge.reduce((acc, cur) => (acc += cur), 0);
 
     if (weightOnBridge + truck_weights[0] <= weight) {
       truckOnBridge.push(truck_weights.shift());
       truckOnBridgeTime.push(bridge_length);
     }
 
-    minTime += 1;
+    result += 1;
   }
-  return minTime;
+
+  return result;
 }
