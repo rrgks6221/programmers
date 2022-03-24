3aa997167f42e1b0c8ebbbf1e42a91d4dce253a3
lsh <rrgks@naver.com>
2022-03-24 21:05:08 +0900

    오픈채팅방 풀이

--- 2단계/오픈채팅방.js
+++ 2단계/오픈채팅방.js
@@ -1,13 +1,21 @@
 function solution(record) {
-    const answer = [];
-    const userdatas = {};
-    const userlog = {Enter: "님이 들어왔습니다.", Leave: "님이 나갔습니다."};
-    for (let i = 0; i < record.length; i++) {
-        record[i] = record[i].split(" ");
-        if (record[i][0] !== "Leave") userdatas[record[i][1]] = record[i][2];
+  const answer = [];
+  const userinfo = {};
+  const userlog = { Enter: '님이 들어왔습니다.', Leave: '님이 나갔습니다.' };
+
+  for (let i = 0; i < record.length; i += 1) {
+    record[i] = record[i].split(' ');
+
+    if (record[i][0] !== 'Leave') {
+      userinfo[record[i][1]] = record[i][2];
     }
-    for (let i = 0; i < record.length; i++) {
-        if (record[i][0] !== "Change") answer.push(userdatas[record[i][1]] + userlog[record[i][0]]);
+  }
+
+  for (let i = 0; i < record.length; i += 1) {
+    if (record[i][0] !== 'Change') {
+      answer.push(userinfo[record[i][1]] + userlog[record[i][0]]);
     }
-    return answer;
+  }
+
+  return answer;
 }
