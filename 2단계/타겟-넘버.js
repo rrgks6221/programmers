110da8bbc63a54a78037d3032f6c4192856e7fb4
lsh <rrgks@naver.com>
2022-03-28 18:51:22 +0900

    타겟-넘버 풀이

--- 2단계/타겟-넘버.js
+++ 2단계/타겟-넘버.js
@@ -1,17 +1,14 @@
 function solution(numbers, target) {
   let result = 0;
 
-  function cal(count, sum) {
-    console.log(sum);
+  (function cal(count, sum) {
     if (count === numbers.length) {
       if (sum === target) result += 1;
       return;
     }
     cal(count + 1, sum + numbers[count]);
     cal(count + 1, sum - numbers[count]);
-  }
-
-  cal(0, 0);
+  })(0, 0);
 
   return result;
 }
