85fa776aaef6c47b599e4a3955e77cac6d29943f
lsh <rrgks@naver.com>
2022-03-29 17:15:47 +0900

    튜플 풀이 수정

--- 2단계/튜플.js
+++ 2단계/튜플.js
@@ -1,23 +1,28 @@
 function solution(s) {
-    const answer = [];
-    let temp = 0;
-    s = s.split("},{");
-    for (let i = 0; i < s.length; i++) {
-        s[i] = s[i].replace(/{|}/g, "");
-        s[i] = s[i].split(",");
+  const answer = [];
+  let temp = 0;
+
+  s = s.split('},{');
+
+  for (let i = 0; i < s.length; i += 1) {
+    s[i] = s[i].replace(/{|}/g, '');
+    s[i] = s[i].split(',');
+  }
+
+  s.sort((a, b) => a.length - b.length);
+
+  for (let i = 0; i < s.length; i += 1) {
+    for (let j = 0; j < s[i].length; j += 1) {
+      s[i][j] = parseInt(s[i][j]);
     }
-    s.sort((a, b) => {
-        return a.length - b.length;
-    })
-    for (let i = 0; i < s.length; i++) {
-        for (let j = 0; j < s[i].length; j++) s[i][j] = parseInt(s[i][j]);
-    }
-    for (let i of s) {
-        let sum = i.reduce((stack, el) => {
-        return stack + el;
-        }, 0);
-        answer.push(sum - temp);
-        temp = sum;
-    }
-    return answer;
+  }
+
+  for (let i of s) {
+    const sum = i.reduce((stack, el) => stack + el, 0);
+
+    answer.push(sum - temp);
+    temp = sum;
+  }
+
+  return answer;
 }
