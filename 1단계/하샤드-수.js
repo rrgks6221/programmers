e68bf0244501cc6678040919ebc7edefeff9ce24
lsh <rrgks@naver.com>
2022-05-14 23:20:37 +0900

    하샤드-수 풀이 수정:

--- 1단계/하샤드-수.js
+++ 1단계/하샤드-수.js
@@ -1,6 +1,8 @@
 function solution(x) {
   const numArr = String(x).split('');
   let sum = 0;
-  for (let i of numArr) sum += Number(i);
+
+  for (const num of numArr) sum += Number(num);
+
   return (x / sum) % 1 === 0;
 }
