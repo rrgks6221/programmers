66529332c02fd5b124fbacd23e3ef2fe3426b3c4
LeeSeokHo <rrgks@naver.com>
2023-02-07 20:30:55 +0900

    Solve: 조건에-맞게-수열-변환하기-1.js

---
+++ 0단계/조건에-맞게-수열-변환하기-1.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+  return arr.map((el, idx) => {
+    if (el >= 50 && el % 2 === 0) {
+      return el / 2;
+    }
+    if (el <= 50 && el % 2 === 1) {
+      return el * 2;
+    }
+    return el;
+  });
+}
