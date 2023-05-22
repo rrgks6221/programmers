f5668c849eec3c747f92144d0e89feaccdd575b7
rrgks6221 <46591459+rrgks6221@users.noreply.github.com>
2023-05-22 21:32:56 +0900

    비밀지도

--- 1단계/[1차]-비밀지도.js
+++ 1단계/[1차]-비밀지도.js
@@ -1,14 +1,26 @@
 function solution(n, arr1, arr2) {
-  const myArray = []
-  for (let i = 0; i < n; i++) {
-      let temp = ""
-      for (let j = 0; j < n; j++) {
-          temp = ((arr1[i] % 2) + (arr2[i] % 2)) + temp;
-          arr1[i] = parseInt(arr1[i] / 2);
-          arr2[i] = parseInt(arr2[i] / 2);
-      }
-      myArray.push(temp);
-      myArray[i] = myArray[i].replace(/0/g, " ").replace(/[1-2]/g, "#");
-  }
-  return myArray;
+  return arr1
+    .map((el, idx) => {
+      return (el | arr2[idx]).toString(2).padStart(n, '0');
+    })
+    .map((el) => {
+      return el.replace(/(0|1)/g, (str) => {
+        return str === '1' ? '#' : ' ';
+      });
+    });
+  const convertBinary = (el) => {
+    return el.toString(2).padStart(n, '0');
+  };
+  const binaryMap1 = arr1.map(convertBinary);
+  const binaryMap2 = arr2.map(convertBinary);
+  return Array.from({ length: n }).map((_, idx) => {
+    let str = '';
+
+    for (let i = 0; i < n; i += 1) {
+      str +=
+        Number(binaryMap1[idx][i]) + Number(binaryMap2[idx][i]) ? '#' : ' ';
+    }
+
+    return str;
+  });
 }
