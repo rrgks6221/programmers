0acd73741cc5940e2d56519ab07127c690c189c7
LeeSeokHo <rrgks@naver.com>
2023-08-12 20:55:26 +0900

    커피 심부름

---
+++ 0단계/커피-심부름.js
@@ -0,0 +1,19 @@
+function solution(order) {
+  const coffeeFee = {
+    iceamericano: 4500,
+    americanoice: 4500,
+    hotamericano: 4500,
+    americanohot: 4500,
+    icecafelatte: 5000,
+    cafelatteice: 5000,
+    hotcafelatte: 5000,
+    cafelattehot: 5000,
+    americano: 4500,
+    cafelatte: 5000,
+    anything: 4500,
+  };
+
+  return order.reduce((acc, cur) => {
+    return acc + coffeeFee[cur];
+  }, 0);
+}
