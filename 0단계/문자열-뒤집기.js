4bc44477278225b090191a5585d933651b1a1741
LeeSeokHo <rrgks@naver.com>
2024-01-01 20:57:23 +0900

    Solve: 문자열-뒤집기.js

--- 0단계/문자열-뒤집기.js
+++ 0단계/문자열-뒤집기.js
@@ -1,8 +1,3 @@
-function solution(my_string, s, e) {
-  const left = my_string.slice(0, s);
-  const middle = my_string.slice(s, e + 1);
-  const right = my_string.slice(e + 1);
-  const reverseMiddle = middle.split('').reverse().join('');
-
-  return left + reverseMiddle + right;
+function solution(my_string) {
+  return my_string.split('').reverse().join('');
 }
