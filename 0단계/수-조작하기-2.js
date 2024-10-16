70740cf841e80ba747db83fd6b1fcb9099598323
LeeSeokHo <rrgks@naver.com>
2024-10-16 21:28:35 +0900

    Solve: 수-조작하기-2.js

---
+++ 0단계/수-조작하기-2.js
@@ -0,0 +1,15 @@
+function solution(numLog) {
+  const obj = {
+    1: 'w',
+    '-1': 's',
+    10: 'd',
+    '-10': 'a',
+  };
+  let result = '';
+
+  for (let i = 0; i < numLog.length - 1; i += 1) {
+    result += obj[String(numLog[i + 1] - numLog[i])];
+  }
+
+  return result;
+}
