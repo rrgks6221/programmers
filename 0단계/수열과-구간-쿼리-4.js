41432b4dbaa50460b41af907f0d0fb9faed42a76
LeeSeokHo <rrgks@naver.com>
2024-11-05 21:02:13 +0900

    Solve: 수열과-구간-쿼리-4.js

---
+++ 0단계/수열과-구간-쿼리-4.js
@@ -0,0 +1,11 @@
+function solution(arr, queries) {
+  queries.forEach(([s, e, k]) => {
+    for (let i = s; i <= e; i += 1) {
+      if (i % k === 0) {
+        arr[i] += 1;
+      }
+    }
+  });
+
+  return arr;
+}
